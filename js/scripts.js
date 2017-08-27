c4e5b9b133e71ef0e3836a1ffc6bb49d0396793b
John <jonnygovish@gmail.com>
2017-08-27 18:13:11 +0300

    Display email confrim to the user that their email has been recieved

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,10 @@ $(document).ready(function () {
         event.preventDefault();
         $('.overlay').removeClass('open');
     });
+    $("form").submit(function(event){
+        event.preventDefault();
+        var userEmail =$("#email").val();
+        console.log(userEmail);
+        alert(userEmail + " has been successfully added to our email list. Thank you.")
+    })
 });
